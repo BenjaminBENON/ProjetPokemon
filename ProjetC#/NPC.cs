5902c28eba34fc5f0d8a412496ea8427c29c164e
BenjaminBENON <benjaminbenon@gaming.tech>
2024-03-28 10:51:20 +0100

    ooopsie

--- ProjetC#/NPC.cs
+++ ProjetC#/NPC.cs
@@ -32,7 +32,7 @@ public  class NPC
 
         for (int i = 0; i < list.Count; i++)
         {
-            Console.SetCursorPosition(xPosition, yPosition + i*2);
+            Console.SetCursorPosition(xPosition, yPosition + i * 2);
             Console.WriteLine(list[i]);
             Console.SetCursorPosition(xPosition, yPosition + i * 2 + 1);
             Console.Read();
